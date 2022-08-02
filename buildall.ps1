0866a15854fb502cedbfb3d6a5f3b4e1f8bd5feb
Jean-Paul Lizotte <jean-paul.lizotte@emyode.com>
2022-08-02 18:00:10 -0400

    Test cases

--- buildall.ps1
+++ buildall.ps1
@@ -1,8 +1,39 @@
-#buildall
-#dotnet new tool-manifest --force
-#dotnet tool install dotnet-ef
-dotnet tool install --global dotnet-sonarscanner --version 5.4.1
-nuget restore
-dotnet sonarscanner begin /key:"GOAT" /name:"GOAT" /d:sonar.host.url="http://20.116.21.79/" /d:sonar.login="d432b0cebbe841766e9e43faa44c7f56fc6ba86c" /version:0.0.0.8 /d:sonar.cs.vscoveragexml.reportsPaths="%temp%\*.coveragexml" /d:sonar.exclusions='**/wwwroot/lib/**'
-msbuild .\WebGoat.NET.sln
-dotnet sonarscanner end /d:sonar.login="d432b0cebbe841766e9e43faa44c7f56fc6ba86c"
+
+$vmname = "vm-sq-sonarqube"
+$sqlurl = "http://emyodesonar-dar.canadacentral.cloudapp.azure.com/"
+$resourcegname = "rg-dar-sonarqube"
+$sqtoken= Get-Content "C:\work\code\OWASP-WebGoat\token.txt"
+
+
+ cd C:\work\code\emyodesonar\SonarQubeScripts
+
+ (Get-AzVM -ResourceGroupName $resourcegname -Name $vmname -Status).Statuses[-1].DisplayStatus
+
+ .\start_stopvm.ps1 -vmname $vmname -resourcegroup $resourcegname
+ if ($LASTEXITCODE -eq 0) {
+   Write-Host "VM $vmname started"
+ } else {
+   Write-Host "VM $vmname failed to start"
+ exit 1
+}
+
+ .\open_nsg_firewall_vm.ps1 -vmname $vmname -resourcegroupname $resourcegname -sonarurl $sqlurl
+if ($LASTEXITCODE -eq 0) {
+   Write-Host "NSG firewall opened"
+} else {
+   Write-Host "NSG firewall failed to open"
+ exit 1
+}
+
+
+# cd C:\work\code\OWASP-WebGoat
+# dotnet tool install --global dotnet-sonarscanner 
+# nuget restore
+# dotnet sonarscanner begin /key:"GOAT" /name:"GOAT" /d:sonar.host.url="$sqlurl" /d:sonar.login="$sqtoken" /version:1.0.0.22 /d:sonar.cs.vscoveragexml.reportsPaths="%temp%\*.coveragexml" /d:sonar.exclusions='**/wwwroot/lib/**'
+# msbuild .\WebGoat.NET.sln
+# dotnet sonarscanner end /d:sonar.login="$sqtoken"
+
+cd C:\work\code\emyodesonar\SonarQubeScripts
+
+.\remove_nsg_firewall_vm.ps1 -vmname $vmname -resourcegroupname $resourcegname
+.\start_stopvm.ps1 -vmname $vmname -resourcegroup $resourcegname -shutdown
